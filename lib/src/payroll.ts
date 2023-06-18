1fc591a41692f6e56ceb0c884dbb04380f46c22f
zian <mynameiszian@gmail.com>
2023-06-18 20:45:05 +0800

    add docs to payroll

--- lib/src/payroll.ts
+++ lib/src/payroll.ts
@@ -1,4 +1,4 @@
-import { BigNumberish, Signer, BytesLike } from "ethers";
+import { BigNumberish, Signer, BytesLike, ContractReceipt } from "ethers";
 import * as payrollContract from "./types/contracts";
 
 const MOONBEAM_PRECOMPILE_CONTRACT_ADDRESS =
@@ -7,23 +7,55 @@ const MOONBEAM_PRECOMPILE_CONTRACT_ADDRESS =
 const DEFAULT_CALLDATA: BytesLike[] = [];
 const DEFAULT_GAS_LIMIT: BigNumberish[] = [];
 
+/**
+ *  payroll. this class is used for sending payroll to employees.
+ *  you should prefer using payrollBuilder for building a new instance of this class.
+ *
+ *  ## Example
+ *
+ *  ```typescript
+ * function buildPayroll(signer: Signer, employees: Employee[] | Employee) {
+ *   const payrollBuilder = new PayrollBuilder()
+ *   payrollBuilder.setSigner(signer)
+ *   payrollBuilder.addEmployee(employees)
+ *
+ *   const payroll  = payroll.build();
+ *
+ *   // dry run the transaction
+ *   await payroll.dry_run()
+ *
+ *   // execute the transaction
+ *   await payroll.execute()
+ * }
+ *  ```
+ */
 export class Payroll {
   private precompile_contract_address: string;
   private signer: Signer;
   private to: string[];
   private value: BigNumberish[];
+  private contractInstance: payrollContract.BatchPrecompileInterface;
 
   constructor(signer: Signer, to: string[], value: BigNumberish[]) {
     this.precompile_contract_address = MOONBEAM_PRECOMPILE_CONTRACT_ADDRESS;
     this.signer = signer;
     this.to = to;
     this.value = value;
+    this.contractInstance = this.connectToContract();
   }
 
-  public async execute() {
+  /**
+   * execute payroll batch transaction. this method will send payroll to all employees.
+   *
+   * this method uses batchAll method from BatchPrecompile contract.
+   * means that if one of the transaction fails to executes, all of the transactions will be reverted.
+   *
+   * @param dryRun will dry run the transaction first if set to true.
+   */
+  public async execute(dryRun = true): Promise<ContractReceipt> {
     const contract = this.connectToContract();
 
-    await this.dry_run(contract, this.to, this.value);
+    if (dryRun) await this.dry_run();
 
     const send_payroll = await contract.batchAll(
       this.to,
@@ -37,14 +69,16 @@ export class Payroll {
     return receipt;
   }
 
-  private dry_run(
-    contract: payrollContract.BatchPrecompileInterface,
-    to: string[],
-    value: BigNumberish[]
-  ) {
-    return contract.callStatic.batchAll(
-      to,
-      value,
+  /**
+   *
+   * dry run the transaction.
+   * will throw an error if one of the transaction fails to execute.
+   *
+   */
+  public async dry_run() {
+    return this.contractInstance.callStatic.batchAll(
+      this.to,
+      this.value,
       DEFAULT_CALLDATA,
       DEFAULT_GAS_LIMIT
     );
